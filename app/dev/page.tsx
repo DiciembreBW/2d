4cbbe94cb2aeabe6107329b0a75217d31f0fc7af
mac <mac@macs-Mac-mini.local>
2023-06-18 08:26:54 +0700

    2

--- app/dev/page.tsx
+++ app/dev/page.tsx
@@ -1,77 +1,84 @@
-"use client";
-import React, {useEffect, useState} from "react";
-import firebase from "@/libs/firebase/firebase";
-import GetData from "./GetData";
+import React from "react";
 
 type Props = {};
-type DataType = {
-	name: string;
-	id?: string;
-	lastname: string;
-};
-
-const db = firebase<DataType>({colName: "/cities"});
 
 export default function page({}: Props) {
-	const data = useState<DataType>({name: "", lastname: ""});
-
-	function onsave() {
-		db.SetDoc({data: data[0]});
-	}
-
-	return (
-		<div className="grid grid-cols-1 gap-2">
-			<div className=" px-3 py-2 flex gap-1 flex-col">
-				<Input state={data} key_name="name" value={data[0].name} />
-				<Input state={data} key_name="lastname" value={data[0].lastname} />
-				<Button onclick={onsave}>​Save</Button>
-			</div>
-
-			{/* preview data */}
-			<GetData />
-		</div>
-	);
+	return <div>page</div>;
 }
+// "use client";
+// import React, {useEffect, useState} from "react";
+// import firebase from "@/libs/firebase/firebase";
+// import GetData from "./GetData";
 
-function Button({
-	onclick,
-	children,
-}: {
-	onclick: Function;
-	children: React.ReactNode;
-}) {
-	return (
-		<button
-			className="px-3 py-2  font-bold rounded w-full bg-emerald-500 text-slate-700"
-			onClick={() => onclick()}>
-			{children}
-		</button>
-	);
-}
+// type Props = {};
+// type DataType = {
+// 	name: string;
+// 	id?: string;
+// 	lastname: string;
+// };
 
-function Input({
-	// state: {data, setData},
-	key_name,
-	value,
-	state: [data, setData],
-}: {
-	state: [DataType, Function];
-	// data: DataType;
-	// setData: Function;
-	key_name: string;
-	value: string;
-}) {
-	return (
-		<div>
-			<input
-				type="text"
-				placeholder={key_name}
-				className="border p-2 focus:outline-none w-full"
-				onChange={(e) => {
-					setData({...data, [key_name]: e.target.value});
-				}}
-				value={value}
-			/>
-		</div>
-	);
-}
+// const db = firebase<DataType>({colName: "/cities"});
+
+// export default function page({}: Props) {
+// 	const data = useState<DataType>({name: "", lastname: ""});
+
+// 	function onsave() {
+// 		db.SetDoc({data: data[0]});
+// 	}
+
+// 	return (
+// 		<div className="grid grid-cols-1 gap-2">
+// 			<div className=" px-3 py-2 flex gap-1 flex-col">
+// 				<Input state={data} key_name="name" value={data[0].name} />
+// 				<Input state={data} key_name="lastname" value={data[0].lastname} />
+// 				<Button onclick={onsave}>​Save</Button>
+// 			</div>
+
+// 			{/* preview data */}
+// 			<GetData />
+// 		</div>
+// 	);
+// }
+
+// function Button({
+// 	onclick,
+// 	children,
+// }: {
+// 	onclick: Function;
+// 	children: React.ReactNode;
+// }) {
+// 	return (
+// 		<button
+// 			className="px-3 py-2  font-bold rounded w-full bg-emerald-500 text-slate-700"
+// 			onClick={() => onclick()}>
+// 			{children}
+// 		</button>
+// 	);
+// }
+
+// function Input({
+// 	// state: {data, setData},
+// 	key_name,
+// 	value,
+// 	state: [data, setData],
+// }: {
+// 	state: [DataType, Function];
+// 	// data: DataType;
+// 	// setData: Function;
+// 	key_name: string;
+// 	value: string;
+// }) {
+// 	return (
+// 		<div>
+// 			<input
+// 				type="text"
+// 				placeholder={key_name}
+// 				className="border p-2 focus:outline-none w-full"
+// 				onChange={(e) => {
+// 					setData({...data, [key_name]: e.target.value});
+// 				}}
+// 				value={value}
+// 			/>
+// 		</div>
+// 	);
+// }
