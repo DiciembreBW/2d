e58a8f99930ee6dccaa835ce8056bdb2a8e8e213
mac <mac@macs-Mac-mini.local>
2023-06-18 19:08:00 +0700

    run build

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,23 @@
-import Link from "next/link";
+// import Link from "next/link";
+import React from "react";
 
-export default function ({children}: {children?: React.ReactNode}) {
-	return (
-		<div className="container bg-slate-300">
-			<div className="flex flex-row w-4/6 mx-auto">
-				<div className="grid grid-cols-3 gap-3">
-					<Link href={"/"}>Home</Link>
-					<Link href={"/test"}>Test</Link>
-				</div>
+type Props = {};
 
-				<div className="grid grid-cols-3">{children}</div>
-			</div>
-		</div>
-	);
+export default function Navbar({}: Props) {
+	return <div>Navbar</div>;
 }
+
+// export default function ({children}: {children?: React.ReactNode}) {
+// 	return (
+// 		<div className="container bg-slate-300">
+// 			<div className="flex flex-row w-4/6 mx-auto">
+// 				<div className="grid grid-cols-3 gap-3">
+// 					<Link href={"/"}>Home</Link>
+// 					<Link href={"/test"}>Test</Link>
+// 				</div>
+
+// 				<div className="grid grid-cols-3">{children}</div>
+// 			</div>
+// 		</div>
+// 	);
+// }
